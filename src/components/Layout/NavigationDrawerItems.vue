6c29a3d46228b5de9dd29b442bcd2fa867d64a90
Grzegorz <grzegorz.sikorski.96@gmail.com>
2019-10-30 13:28:19 +0100

    navigation drawer items

--- src/components/Layout/NavigationDrawerItems.vue
+++ src/components/Layout/NavigationDrawerItems.vue
@@ -1,9 +1,10 @@
 <template>
     <v-list dense shaped>
-        <span v-for="category in links" :key="category.category">
-            <v-subheader class="mt-4 white--text font-weight-bold text--darken-1">{{category.category}}</v-subheader>
+        <span v-for="category in links" :key="category.name">
+            <v-subheader class="mt-4 white--text font-weight-bold text--darken-1">{{category.name}}</v-subheader>
             <span v-for="link in category.links" :key="link.route">
-                <v-list-item ripple link :to="{name: link.route}" class="route" exact>
+                <v-list-item v-if="checkPermitted(link, category)" ripple link :to="{name: link.route}" class="route"
+                             exact>
                     <v-list-item-action>
                         <font-awesome-icon :icon="link.icon" size="lg"/>
                     </v-list-item-action>
@@ -26,31 +27,58 @@
             logged: true,
             links: [
                 {
-                    category: 'Główne', links: [
-                        {route: 'HomePage', label: 'Strona głowna', icon: 'home'},
+                    name: 'Główne', available: 0, links: [
+                        {route: 'HomePage', label: 'Strona głowna', icon: 'home', available: true},
                     ]
                 },
                 {
-                    category: 'Zgłoszenia', links: [
-                        {route: 'TicketCheckStatus', label: 'Sprawdź status zgłoszenia', icon: 'clipboard-check'},
-                        {route: 'TicketsList', label: 'Lista zgłoszeń', icon: 'clipboard-list'},
-                        {route: 'TicketCreate', label: 'Dodaj zgłoszenie', icon: 'plus'},
+                    name: 'Zgłoszenia', available: 0, links: [
+                        {
+                            route: 'TicketCheckStatus',
+                            label: 'Sprawdź status zgłoszenia',
+                            icon: 'clipboard-check',
+                            available: true
+                        },
+                        {route: 'TicketsList', label: 'Lista zgłoszeń', icon: 'clipboard-list', available: true},
+                        {route: 'TicketCreate', label: 'Dodaj zgłoszenie', icon: 'plus', available: true},
                     ]
                 },
                 {
-                    category: 'Klienci', links: [
-                        {route: 'ClientList', label: 'Lista klientów', icon: 'users'},
-                        {route: 'ClientCreate', label: 'Dodaj klienta', icon: 'user-plus'},
+                    name: 'Klienci', available: 0, links: [
+                        {route: 'ClientList', label: 'Lista klientów', icon: 'users', available: true},
+                        {route: 'ClientCreate', label: 'Dodaj klienta', icon: 'user-plus', available: true},
                     ]
                 },
                 {
-                    category: 'Urządzenia', links: [
-                        {route: 'DeviceList', label: 'Lista urządzeń', icon: 'laptop'},
-                        {route: 'DeviceCreate', label: 'Dodaj urządzenie', icon: 'laptop-medical'},
+                    name: 'Urządzenia', available: 0, links: [
+                        {route: 'DeviceList', label: 'Lista urządzeń', icon: 'laptop', available: true},
+                        {route: 'DeviceCreate', label: 'Dodaj urządzenie', icon: 'laptop-medical', available: true},
                     ]
                 }
             ]
         }),
+        methods: {
+            checkPermitted(routeName) {
+                if (this.$router.resolve({name: routeName.route}).route.meta.roles) {
+                    if (this.$store.currentUser) {
+                        return this.$router.resolve({name: routeName}).route.meta.roles.includes(this.$store.state.currentUser.role.name);
+                    }
+                    return false;
+                }
+                return true;
+            },
+        },
+        computed: {
+            check(routeName) {
+                if (this.$router.resolve({name: routeName.route}).route.meta.roles) {
+                    if (this.$store.currentUser) {
+                        return this.$router.resolve({name: routeName}).route.meta.roles.includes(this.$store.state.currentUser.role.name);
+                    }
+                    return false;
+                }
+                return true;
+            },
+        }
     }
 </script>
 
